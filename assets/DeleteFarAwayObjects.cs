93786885f5b7ee98c44fa08fbd4d38b9fbd6bb79
Belbertn <belbert95@me.com>
2014-08-25 03:44:16 +0200

    Comitting bit by bit

---
+++ assets/DeleteFarAwayObjects.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteFarAwayObjects : MonoBehaviour {
+
+	public GameObject player;
+	public float maxDistance = 50;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		object[] objects = GameObject.FindObjectsOfType(typeof (GameObject));
+
+		foreach (GameObject obj in objects) {
+			if((obj.transform.position - player.transform.position).magnitude > maxDistance)
+			{
+				Destroy (obj);
+			}
+		}
+	}
+}
---
+++ assets/DestroysOnCollision.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroysOnCollision : MonoBehaviour {
+
+	bool dead;
+
+	// Use this for initialization
+	void Start () {
+		dead = false;
+	}
+
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+
+		dead = true;
+	}
+
+	void Update()
+	{
+		if (dead)
+			Destroy (gameObject);
+	}
+}
