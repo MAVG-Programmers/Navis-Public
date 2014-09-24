bcab09b44eecd9acb69acd02750d3321c965956e
Njaal Tangye <belbert95@me.com>
2014-09-24 23:32:57 +0200

    New audio script

--- Assets/PlayerShooting.cs
+++ Assets/PlayerShooting.cs
@@ -3,23 +3,32 @@ using System.Collections;
 
 public class PlayerShooting : MonoBehaviour {
 
-	public float fireRate = 1.5f;
-	public float positionOffset = 1.0f;
-	public float bulletSpeed = 10.0f;
+    public float cannonFireRate = 1.5f;
+    public float missileFireRate = 5f;
+    public float positionOffset = 1.0f;
+    public float bulletSpeed = 10.0f;
+    public float missileSpeed = 10.0f;
 
-	float fireTimer;
+    float laserTimer;
+    float missileTimer;
+
+    AudioClips playerAudio;
+
+    public AudioClip laserSound;
+    public AudioClip engineSound;
 
 	// Use this for initialization
 	void Start () {
-		fireTimer = 0.0f;
+		laserTimer = 0.0f;
+        playerAudio = GetComponent<AudioClips>();
 	}
 	
 
 	void Update () {
 
-		fireTimer += Time.deltaTime;
+		laserTimer += Time.deltaTime;
 
-		if (Input.GetKey (KeyCode.Space) && fireTimer > fireRate) 
+		if (Input.GetKey (KeyCode.Space) && laserTimer > cannonFireRate) 
 		{
 			GameObject bullet = (GameObject) Instantiate (Resources.Load ("GLaser"), 
 			                                              transform.position + transform.up * positionOffset, 
@@ -28,9 +37,9 @@ public class PlayerShooting : MonoBehaviour {
 			bullet.rigidbody2D.velocity = bulletSpeed * transform.up;
 			bullet.rigidbody2D.drag = 0.0f;
 
-			fireTimer = 0.0f;
+			laserTimer = 0.0f;
 
-            audio.Play();
+            playerAudio.PlayClipOneShot(laserSound);
 		}
 	}
 }
