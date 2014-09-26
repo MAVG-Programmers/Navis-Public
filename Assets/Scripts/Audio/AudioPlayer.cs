c8d7d7a4e7e6466171e842942b05f9b54f068f6e
Njaal Tangye <belbert95@me.com>
2014-09-26 21:06:30 +0200

    Audio

--- Assets/Scripts/Audio/AudioPlayer.cs
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AudioPlayer : MonoBehaviour {
+public class AudioPlayer : MonoBehaviour
+{
+    AudioClip clip { set; }
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 	
 	}
 }
