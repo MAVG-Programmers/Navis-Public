0c1297bdc9ffadfbc86bc2300d6704d1536a0c34
Njaal Tangye <belbert95@me.com>
2014-10-09 21:41:05 +0200

    hi

---
+++ Assets/EnemyNav.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyNav : MonoBehaviour {
+	public Transform target;
+
+	NavMeshAgent agent;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.destination = target.position;
+	}
+}
